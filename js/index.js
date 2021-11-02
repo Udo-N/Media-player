101a6ae88bb7eaafdaeee8b4902587ce7cefb9a8
Udo-N <udoka404@gmail.com>
2021-11-02 05:06:08 -0400

    Work in progress

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+function load(url){
+
+}
+
 function createPlayer (divId, width, height){
     div = document.getElementById(divId);
 
@@ -5,17 +9,17 @@ function createPlayer (divId, width, height){
     text += " width='"+ width +"' height='"+ height +"'>";
     text += "<source src='media/Bamboozle 7.mp4' type='video/mp4'>";
     text +=  "Sorry, your browser doesn't support embedded videos.</video>";
-    text += "<button id = 'toggle-play'></button>";
+    // text += "<button id = 'toggle-play'></button>";
     div.innerHTML = text;
 
-    const video = document.getElementById('video');
-    const videoControls = document.getElementById('video-controls');
+    // const video = document.getElementById('video');
+    // const videoControls = document.getElementById('video-controls');
     
-    const videoWorks = !!document.createElement('video').canPlayType;
-    if(videoWorks) {
-        video.controls = false;
-        videoControls.classList.remove('hidden');
-    }
+    // const videoWorks = !!document.createElement('video').canPlayType;
+    // if(videoWorks) {
+    //     video.controls = false;
+    //     videoControls.classList.remove('hidden');
+    // }
 }
 
 function play(){
@@ -40,22 +44,80 @@ function getWidth(){
 }
 
 function setAutoplay(autoplay){
-    
+    if (autoplay){
+        video.autoplay = true;
+    }
+    else{
+        video.autoplay = false;
+    }
+}
+
+function setVolume(volume){
+    video.volume = volume/100;
+
+}
+
+function getVolume(){
+    return video.volume;
+}
+
+function setMute(mute){
+    if (mute){
+        video.muted = true;
+    }
+    else{
+        video.muted = false;
+    }
+}
+
+function getDuration(){
+    return parseInt(video.duration);
+}
+
+function setFullscreen(fullscreen){
+    if (fullscreen){
+        video.requestFullscreen();
+    }
+    else{
+        video.fullscreen = false;
+    }
+}
+
+function getPlaybackState(){
+    var duration = video.duration;
+    if (video.paused && video.currentTime != duration){
+        return "Paused"
+    }
+    else if (!video.paused && video.currentTime < duration){
+        return "Playing"
+    }
+    else if (video.paused && video.currentTime == duration){
+        return "Ended"
+    }
+}
+
+function getViewability(){
+
 }
 
 createPlayer("video-player", 500, 300);
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
+// setFullscreen(true);
+// console.log(video.duration);
+setInterval(function(){console.log(getPlaybackState());}, 2000);
+// setInterval(function(){console.log(video.currentTime);}, 2000);
 
-document.addEventListener("DOMContentLoaded", () => {
-    const togglePlay = document.querySelector("#toggle-play");
 
-    togglePlay.addEventListener("click", () => {
-        console.log(getWidth());
-        if (video.paused || video.ended) {
-            play();
-        }else {
-            pause();
-        }
-    });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const togglePlay = document.querySelector("#toggle-play");
+
+//     togglePlay.addEventListener("click", () => {
+//         console.log(getWidth());
+//         if (video.paused || video.ended) {
+//             play();
+//         }else {
+//             pause();
+//         }
+//     });
+// });
